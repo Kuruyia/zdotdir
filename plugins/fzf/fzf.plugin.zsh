33e1c2f8a3772044aba1b7112fb1b2d7595629b2
Kuruyia <github@kuruyia.net>
2024-04-29 23:50:29 +0200

    feat(fzf): support completions on macOS

--- plugins/fzf/fzf.plugin.zsh
+++ plugins/fzf/fzf.plugin.zsh
@@ -2,8 +2,13 @@
 # fzf - Sources the fzf init script
 #
 
-FZF_KEY_BINDINGS_SCRIPT_PATH="/usr/share/fzf/key-bindings.zsh"
-FZF_COMPLETION_SCRIPT_PATH="/usr/share/fzf/completion.zsh"
+if is-macos; then
+  FZF_KEY_BINDINGS_SCRIPT_PATH="/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+  FZF_COMPLETION_SCRIPT_PATH="/opt/homebrew/opt/fzf/shell/completion.zsh"
+else
+  FZF_KEY_BINDINGS_SCRIPT_PATH="/usr/share/fzf/key-bindings.zsh"
+  FZF_COMPLETION_SCRIPT_PATH="/usr/share/fzf/completion.zsh"
+fi
 
 [[ -s "$FZF_KEY_BINDINGS_SCRIPT_PATH" ]] && source "$FZF_KEY_BINDINGS_SCRIPT_PATH"
 [[ -s "$FZF_COMPLETION_SCRIPT_PATH" ]] && source "$FZF_COMPLETION_SCRIPT_PATH"
